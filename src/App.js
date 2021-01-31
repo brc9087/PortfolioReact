1f6420f43a12227918c445583e834e48b0099a83
Brian Cardines <bcardines811@gmail.com>
2021-01-31 17:51:46 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
@@ -13,9 +13,11 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
+          <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/search" component={Search} />
+          </Switch>
         </Wrapper>
         <Footer />
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,39 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
+// import "./style.css";
+
+const styles = {
+  navApp: {
+    backgroundColor: "blue",
+    color: "white",
+    fontSize: "20px",
+    padding: "20px",
+    borderBottom: "7px rgb(247, 39, 184)"
+  },
+  
+  whiteFont: {
+    color: "white"
+  }
+}
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Wikipedia Searcher
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/home"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/signup"
-              className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}
-            >
-              Sign Up
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div style={styles.navApp} className="navApp">
+      <nav className="navbar">
+        <div className="col-md-1"></div>
+        <div className="col-md-3">
+          <span id="name" className="navbar-brand mb-0 h1">BRIAN CARDINES</span>
+        </div>
+        <div className="col-md-3">
+        </div>
+
+        <div className="col-md-1">
+          <Link to="/About" style={styles.whiteFont}>About Me</Link>
+        </div>
+
+        <div className="col-md-1">
+          <Link to="/Portfolio" style={styles.whiteFont}>Portfolio</Link>
+        </div>
+
+        <div className="col-md-1">
+          <Link to="/Contact" style={styles.whiteFont}>Contact</Link>
+        </div>
+
+        <div className="col-md-1"></div>
+      </nav>
+
+    </div>
   );
 }
 
