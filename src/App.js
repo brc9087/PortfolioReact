3c4103814882e2baa4110b8069245818471b1940
Brian Cardines <bcardines811@gmail.com>
2021-01-31 22:41:39 -0500

    portfoliopage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Search from "./pages/Search";
 import About from "./pages/About/index"
+import Portfolio from "./pages/Portfolio/index"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import "./App.css";
+
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
         <Wrapper>
           <Switch>
           <Route exact path="/" component={About} />
-          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/search" component={Search} />
           </Switch>
         </Wrapper>
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,138 @@
+import React from "react";
+// import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import workout from "../../images/workout.png";
+import employee from "../../images/employeedirectory.png";
+import burger from "../../images/burger.png";
+import hole from "../../images/hole.png";
+import nba from "../../images/nba.png";
+import note from "../../images/note.png"
+
+const styles = {
+    back: {
+        backgroundColor: "grey"
+    }
+}
+
+const portfolio = () => {
+    return (
+        <div className="portfolioApp">
+            <div style={styles.back} className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>
+                            Portfolio
+                    </h3>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-1">
+                            </div>
+
+                            <div className="col-md-3">
+                                <a href="https://boiling-cliffs-25283.herokuapp.com/">
+                                    <img src={workout}
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">WORKOUT TRACKER</p>
+                                <p id="repo"><a href="https://github.com/brc9087/WorkOutTrackerHW">* Workout Tracker Repo</a></p>
+                            </div>
+                            <div className="col-md-1"></div>
+
+
+                            <div className="col-md-3">
+                                <a href="https://brc9087.github.io/myapp/ ">
+                                    <img src={employee}
+                                        className="card-img-top" alt="img-two" />
+                                </a>
+                                <p className="card-text">EMPLOYEE DIRECTORY REACT</p>
+                                <p id="repo"><a href="https://github.com/brc9087/EmployeeReactDirectory"
+                                >* Empoyee Directory React Repo </a></p>
+                            </div>
+                        
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className="row">
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-3">
+                                <a href="https://immense-waters-93242.herokuapp.com/">
+                                    <img src={burger}
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">EAT DA BURGER</p>
+                                <p id="repo"><a href="https://github.com/brc9087/Eat-Da-BurgerHW"
+                                >* Eat Da Burger Repo</a></p>
+                            </div>
+                            <div className="col-md-1">
+
+
+                            </div>
+                            <div className="col-md-3">
+                                <a href="https://joemc9011.github.io/emorph/">
+                                    <img src={hole}
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text"> HOLE IN THE WALL (project 1) </p>
+                                <p id="repo"><a href="https://github.com/brc9087/HoleInTheWall"
+                                >* Hole In The Wall Repo</a></p>
+                                <div className="col-md-1"></div>
+                            </div>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <div className="row">
+                            <div className="col-md-1"></div>
+                            <div className="col-md-3">
+                                <a href="https://fast-dawn-07792.herokuapp.com/">
+                                    <img src={nba}
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">MY TEAM (project 2)</p>
+                                <p id="repo"><a href="https://github.com/brc9087/MyTeam"
+                                >* My Team Repo</a></p>
+
+                            </div>
+
+                            <div className="col-md-1"></div>
+
+                            <div className="col-md-3">
+                                <a href="https://mighty-headland-17179.herokuapp.com/">
+                                    <img src={note}
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">NOTETAKER APP</p>
+                                <p id="repo"><a href="https://github.com/brc9087/NoteTaker"
+                                >* Notetaker App Repo</a></p>
+
+                            </div>
+                        </div>
+                        <br></br>
+                        <div className="gitProfile">
+                            <p>GitHub Profile: <a href="https://github.com/brc9087">
+                                https://github.com/brc9087</a> </p>
+                            <p>Linkedin Profile: <a href="https://www.linkedin.com/feed/"
+                            >Linkedin Profile</a></p>
+                            <p>Email Address: bcardines811@gmail.com</p>
+                            <p>Phone Number:908-405-9820</p>
+                            <a href="https://github.com/Ana199816/homework-16/blob/master/assets/resume/resume.pdf"
+                            > Resume</a>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
+export default portfolio
